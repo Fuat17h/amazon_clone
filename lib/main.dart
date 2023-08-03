bfa4c3def9779daf7647623806aa229a4e77a76b
Fuat17h <fuat17h@gmail.com>
2023-08-03 21:32:21 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:amazon_clone/constants/global_variables.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Amazon Clone',
+      theme: ThemeData(
+        scaffoldBackgroundColor: GlobalVariables.backgroundColor,
+        colorScheme: const ColorScheme.light(
+          primary: GlobalVariables.secondaryColor,
+        ),
+        appBarTheme: const AppBarTheme(
+            elevation: 0, iconTheme: IconThemeData(color: Colors.black)),
+        primarySwatch: Colors.blue,
+      ),
+      // onGenerateRoute: ,
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Hello.'),
+          ),
+          body: Column(
+            children: [
+              const Center(
+                child: Text('Flutter Demo Home Page'),
+              ),
+              ElevatedButton(onPressed: () {}, child: const Text('Click'))
+            ],
+          )),
+    );
+  }
+}
